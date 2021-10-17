433d62bf392e778f35ae2456a711ae1a54dc5fd6
Paulo Deyvid <88635207+PDEYVID@users.noreply.github.com>
2021-10-17 13:46:55 -0300

    Create bloco_codigos.js

---
+++ Inicio/comentario.js
@@ -0,0 +1,11 @@
+// console.log e usado pra exibir algo na tela do computador! 
+console.log("Passo 01");
+console.log("Passo 02"); // mais um exemplo de console.log!
+//console.log("Passo 03");
+
+
+/*
+ este e um comenyario de multiplas linhas 
+ *
+*/
+console.log('Passo #02') 
